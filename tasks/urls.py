f7eae9d5917e37801acbbb0e480e094c25f14797
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-12-15 20:13:40 +0200

    add url for Task end-point

---
+++ tasks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from tasks import views
+
+urlpatterns = [
+    path('', views.TaskListView.as_view(), name="tasks-list"),
+    path('<int:pk>/', views.TaskDetailView.as_view(), name="task-detail"),
+    path('<int:pk>/update', views.TaskUpdateView.as_view(), name="task-update"),
+    path('<int:pk>/delete', views.TaskDeleteView.as_view(), name="task-delete"),
+    path('task-create/', views.TaskCreationView.as_view(), name="task-create"),
+    path("<int:pk>/complete/", views.TaskCompleteView.as_view(), name="task-complete"),
+
+]
+
+app_name = "tasks"
